a89090a1afcc129c4efc6495c717baeb10c15a90
ilyabudkovoy <ilyabudkovoy@yandex.ru>
2026-04-30 18:40:19 +0300

    feat(catalog): add item detail page with buy button flow (#3)

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from apps.core.views import healthcheck
 
@@ -7,4 +7,5 @@ from apps.core.views import healthcheck
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", healthcheck, name="healthcheck"),
+    path("", include("apps.catalog.urls")),
 ]
